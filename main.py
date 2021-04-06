22d3fee851ebaa8cf0ec5107b17a6a31f81046a0
Thrshan <thrshan.jeevaraj@gmail.com>
2021-04-06 19:10:13 +0530

    Update for raspberry pi 4B

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium.webdriver import Chrome
+from selenium import webdriver
 from datetime import datetime
 import sql_handler
 import time
@@ -7,7 +8,7 @@ def scrub(table_name):
     return ''.join( chr for chr in table_name if chr.isalnum() )
 
 links = []
-with open('links', 'r') as links_file:
+with open('/home/pi/Project/Python/amazon-price-scrapper/links', 'r') as links_file:
     for line in links_file:
         links.append(line.strip())
 
@@ -17,18 +18,24 @@ date = dt.strftime("%Y-%m-%d %H:%M:%S")
 
 
 try:
-    driver = Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--window-size=1420,1080')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    driver = Chrome(chrome_options=chrome_options)
     driver.get(links[0])
     title = driver.find_element_by_css_selector("#productTitle").text
     price = driver.find_element_by_css_selector(".offer-price").text
 
-    db_file = "db/price_list.db"
+    db_file = "/home/pi/Project/Python/amazon-price-scrapper/db/price_list.db"
     data = {
         'date':date,
         'ts':ts,
         'price':float(price.split(' ')[-1])
     }
     sql_handler.insert_data_db(db_file, '"{}"'.format(title), data)
+    print(data)
 finally:
     driver.quit()
 
