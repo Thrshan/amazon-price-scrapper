0c77cc03ff0e074776fc0aacd1a0630b7b1f25a4
Thrshan <thrshan.jeevaraj@gmail.com>
2021-04-08 01:43:56 +0530

    Add config file, Add feature for multiple links

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from selenium.webdriver import Chrome
 from selenium import webdriver
 from datetime import datetime
+import hjson
 import sql_handler
 import os
 
 cwd = os.path.dirname(os.path.realpath(__file__))
+import json
+
+with open(cwd + '/config.hjson') as config_file:
+    config = hjson.load(config_file)
 
 def scrub(table_name):
     return ''.join( chr for chr in table_name if chr.isalnum() )
-
-links = []
-with open(cwd + '/links', 'r') as links_file:
-    for line in links_file:
-        links.append(line.strip())
 
 dt = datetime.now()
 ts = dt.timestamp()
@@ -23,21 +23,33 @@ try:
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--window-size=1420,1080')
-    chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     driver = Chrome(chrome_options=chrome_options)
-    driver.get(links[0])
-    title = driver.find_element_by_css_selector("#productTitle").text
-    price = driver.find_element_by_css_selector(".offer-price").text
+    for productLink in config['productLinks']:
+        driver.get(productLink)
+        title = driver.find_element_by_css_selector("#productTitle").text
+        if len(driver.find_elements_by_css_selector(".offer-price")) > 0:
+            price = driver.find_element_by_css_selector(".offer-price").text
+        else:
+            print(".offer-price not exist")
+            price = driver.find_element_by_css_selector("#priceblock_ourprice").text
 
-    db_file = cwd + '/db/price_list.db'
-    data = {
-        'date':date,
-        'ts':ts,
-        'price':float(price.split(' ')[-1])
-    }
-    sql_handler.insert_data_db(db_file, '"{}"'.format(title), data)
-    print(data)
+        print(price)
+        price = price.replace("₹","").replace(",","")
+        print(price)
+        price = float(price.strip())
+        print(price)
+
+        db_file = cwd + '/db/' + config['dbFile'] if config['dbPath'] == "" else config['dbPath'] + "/" + config['dbFile']
+        
+        data = {
+            'date':date,
+            'ts':ts,
+            'price':price
+        }
+        sql_handler.insert_data_db(db_file, '"{}"'.format(title), data)
+        print(data)
 finally:
     driver.quit()
 
--- sql_handler.py
+++ sql_handler.py
@@ -1,18 +1,22 @@
 import sqlite3
 
-def insert_data_db(db_file, table_name, data):    
-    con = sqlite3.connect(db_file)
-    cur = con.cursor()
-    # # Create table
-    cur.execute('''CREATE TABLE IF NOT EXISTS {}
-                (date text, timestamp real, price real)'''.format(table_name))
+def insert_data_db(db_file, table_name, data):
+    try: 
+        con = sqlite3.connect(db_file)
+        cur = con.cursor()
+        # # Create table
+        cur.execute('''CREATE TABLE IF NOT EXISTS {}
+                    (date text, timestamp real, price real)'''.format(table_name))
 
-    # Insert a row of data
-    cur.execute("INSERT INTO {} VALUES (?,?,?)".format(table_name), (data['date'], data['ts'],data['price']))
+        # Insert a row of data
+        cur.execute("INSERT INTO {} VALUES (?,?,?)".format(table_name), (data['date'], data['ts'],data['price']))
 
-    # # Save (commit) the changes
-    con.commit()
+        # # Save (commit) the changes
+        con.commit()
 
-    # # We can also close the connection if we are done with it.
-    # # Just be sure any changes have been committed or they will be lost.
-    con.close()
+        # # We can also close the connection if we are done with it.
+        # # Just be sure any changes have been committed or they will be lost.
+        con.close()
+    except sqlite3.OperationalError as err:
+        print ("Database error: {}".format(err))
+        # print("dbFile:{}\ntableName:{}\ndata:{}".format(db_file, table_name, data))
